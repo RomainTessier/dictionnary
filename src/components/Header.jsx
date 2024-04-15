7fc536a2ce8e22a7d3bc88e8d12bf0900884270a
RomainTessier <romaintessierpro1@gmail.com>
2024-04-15 15:37:55 +0200

    switch working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,8 +16,8 @@ export default function Header(){
                     Sans serif
                 </div>
                 <div className="darkmode">
-                    <div className="darkmode-switch d-flex ai-center" onClick={() => setDarkMode(!darkMode)}>
-                        <div className="circle"></div>
+                    <div className="darkmode-switch d-flex ai-center" onClick={() => setDarkMode(!darkMode)} style={{backgroundColor : darkMode ? '#A445ED' : '#757575'}}>
+                        <div className={`circle ${darkMode ? 'toggled' : ''}`}></div>
                     </div>
                     <img src={Moon} alt="moon icon"/>
                 </div>
