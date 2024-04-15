e269b30586bbccb7f558fce513b824e49f4cca85
RomainTessier <romaintessierpro1@gmail.com>
2024-04-15 17:37:31 +0200

    header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import Logo from '../assets/images/logo.svg'
 import Moon from '../assets/images/icon-moon.svg'
+import Arrow from '../assets/images/icon-arrow-down.svg'
 import '../style/header.css'
 import '../style/global.css'
 import { useState } from 'react'
@@ -12,11 +13,14 @@ export default function Header(){
         <header className='d-flex ai-center'>
             <img src={Logo} alt="logo"/>
             <div className="custom d-flex" >
-                <div className="font">
-                    Sans serif
+                <div className="font d-flex ai-center">
+                    <div className="fontName">
+                        Sans Serif
+                    </div>
+                    <img src={Arrow} alt="arrow icon"/>
                 </div>
-                <div className="darkmode">
-                    <div className="darkmode-switch d-flex ai-center" onClick={() => setDarkMode(!darkMode)} style={{backgroundColor : darkMode ? '#A445ED' : '#757575'}}>
+                <div className="darkmode d-flex ai-center">
+                    <div className="darkmode-switch" onClick={() => setDarkMode(!darkMode)} style={{backgroundColor : darkMode ? '#A445ED' : '#757575'}}>
                         <div className={`circle ${darkMode ? 'toggled' : ''}`}></div>
                     </div>
                     <img src={Moon} alt="moon icon"/>
