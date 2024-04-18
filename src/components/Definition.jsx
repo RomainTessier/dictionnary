183a2ce6300ea94e2d172f768638dd4cb45d6e62
RomainTessier <romaintessierpro1@gmail.com>
2024-04-18 17:07:15 +0200

    synonyms - example - definition and style

--- src/components/Definition.jsx
+++ src/components/Definition.jsx
@@ -1,7 +1,41 @@
+import '../style/definition.css';
+
 export default function Definition({wordMeaning}) {
     console.log(wordMeaning);
+
+
     return (
-        <>
-        </>
+        <div className="definition">
+            <div className="wordType d-flex ai-center">
+                <h3>{ wordMeaning.partOfSpeech }</h3>
+                <div></div>
+            </div>
+            <div className="meaning">Meaning</div>
+
+            <ul>
+                {wordMeaning.definitions.map((def, index) => {
+                        return (
+                            <li>
+                                {def.definition}
+                            </li>
+                        )
+                    }
+                )}
+                <div className="example" style={{display : wordMeaning.definitions[0].example != undefined ? 'flex' : 'none'}}>
+                    "{wordMeaning.definitions[0].example}"
+                </div>
+            </ul>
+
+            <div className="synonyms" style={{display : wordMeaning.synonyms.length ? 'flex' : 'none'}}>
+                    <div>Synonyms</div>
+                    <p>
+                        {wordMeaning.synonyms.join(', ')}
+                    </p>
+            </div>
+
+            <div className="source">
+
+            </div>
+        </div>
     )
 }
